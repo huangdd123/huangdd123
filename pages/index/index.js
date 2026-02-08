f3a5ca857f855cfe7e409096490fd7971fe35bff
huangdd123 <144088802+huangdd123@users.noreply.github.com>
2026-02-08 19:35:00 +0800

    Add non-heritage mini program pages

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+Page({
+  goMain() {
+    wx.navigateTo({
+      url: '/pages/main/main'
+    })
+  },
+  goRegister() {
+    wx.navigateTo({
+      url: '/pages/register/register'
+    })
+  }
+})
